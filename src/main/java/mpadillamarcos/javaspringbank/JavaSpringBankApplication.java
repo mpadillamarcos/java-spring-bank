ad5dd4bda70cca891f53dc881b4b7083f4c1c1e3
Maria Padilla <maria.pad.mar@gmail.com>
2024-04-06 12:25:02 +0200

    Project scaffolding

---
+++ src/main/java/mpadillamarcos/javaspringbank/JavaSpringBankApplication.java
@@ -0,0 +1,13 @@
+package mpadillamarcos.javaspringbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mpadillamarcos/javaspringbank/JavaSpringBankApplicationTests.java
@@ -0,0 +1,13 @@
+package mpadillamarcos.javaspringbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
