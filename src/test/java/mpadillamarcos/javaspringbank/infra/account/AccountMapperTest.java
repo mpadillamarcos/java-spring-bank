c3a88e5c5399066fb257025cd931b5b5e8874977
Maria Padilla <maria.pad.mar@gmail.com>
2024-04-22 10:52:54 +0200

    Implement AccountMapper

--- src/test/java/mpadillamarcos/javaspringbank/infra/account/AccountMapperTest.java
+++ src/test/java/mpadillamarcos/javaspringbank/infra/account/AccountMapperTest.java
@@ -1,13 +1,18 @@
 package mpadillamarcos.javaspringbank.infra.account;
 
+import mpadillamarcos.javaspringbank.domain.account.Account;
+import mpadillamarcos.javaspringbank.domain.account.AccountId;
 import mpadillamarcos.javaspringbank.infra.MapperTestBase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static mpadillamarcos.javaspringbank.domain.Instances.dummyAccount;
 import static mpadillamarcos.javaspringbank.domain.account.AccountId.randomAccountId;
-import static mpadillamarcos.javaspringbank.infra.TestClock.NOW;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = {AccountMapper.class})
@@ -25,11 +30,39 @@ class AccountMapperTest extends MapperTestBase {
 
     @Test
     void returns_inserted_account() {
-        var account = dummyAccount().createdDate(NOW).build();
+        var account = dummyAccount().build();
         mapper.insert(account);
 
         var storedAccount = mapper.findById(account.getId());
 
         assertThat(storedAccount).hasValue(account);
+    }
+
+    @Test
+    void updates_an_already_inserted_account() {
+        var account = dummyAccount().build();
+        mapper.insert(account);
+
+        mapper.update(account.block());
+
+        var storedAccount = mapper.findById(account.getId());
+
+        assertThat(storedAccount).hasValue(account.block());
+    }
+
+    @Test
+    void returns_a_list_of_accounts() {
+        var account1 = dummyAccount().build();
+        var account2 = dummyAccount().build();
+        mapper.insert(account1);
+        mapper.insert(account2);
+        mapper.insert(dummyAccount().build());
+        Set<AccountId> accountIds = new HashSet<>();
+        accountIds.add(account1.getId());
+        accountIds.add(account2.getId());
+
+        List<Account> accountsList = mapper.getAccounts(accountIds);
+
+        assertThat(accountsList).containsExactly(account1, account2);
     }
 }
